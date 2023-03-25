1feba29ca31cc4397f1018bf9ed7f3b33ce05350
AdrianGrillo <grillo.adrian13@gmail.com>
2023-03-25 13:58:25 -0400

    moar

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,9 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import { MDBInput } from 'mdb-react-ui-kit'
+
+const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
