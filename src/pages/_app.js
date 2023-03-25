a77c710250fc0ac6323fe99797d49e692e2e4070
AdrianGrillo <grillo.adrian13@gmail.com>
2023-03-25 14:24:11 -0400

    boom

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,6 @@
-import '@/styles/globals.css'
+import '@/styles/globals.scss'
+import 'mdb-react-ui-kit/dist/css/mdb.min.css'
+import "@fortawesome/fontawesome-free/css/all.min.css"
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,25 @@
 import Head from 'next/head'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
-import { MDBInput } from 'mdb-react-ui-kit'
-
-const inter = Inter({ subsets: ['latin'] })
+import styles from '@/styles/Home.module.scss'
+import { MDBInput, MDBDropdown, MDBDropdownMenu, MDBDropdownToggle, MDBDropdownItem } from 'mdb-react-ui-kit'
+import { useState } from 'react'
 
 export default function Home() {
+  const [jobTitle, setJobTitle] = useState('')
+  const [currentLocation, setCurrentLocation] = useState('')
+  const [livingType, setLivingType] = useState('')
+
+  const handleJobTitle= (e) => {
+    setJobTitle(e.target.value)
+  }
+
+  const handleCurrentLocation= (e) => {
+    setCurrentLocation(e.target.value)
+  }
+
+  const handleLivingType= (e) => {
+    setLivingType(e.target.textContent)
+  }
+
   return (
     <>
       <Head>
@@ -16,9 +30,16 @@ export default function Home() {
       </Head>
       <main className={styles.main}>
         <div className=' flex form'>
-          <MDBInput label='Your Salary' className='input' type='text' />
-          <MDBInput label='Your Desired Rent Cost' className='input' type='text' />
-          <MDBInput label='Your Desired COL' className='input' type='text' />
+          <MDBInput label='Job Title' className='input' type='text' value={jobTitle} onChange={e => handleJobTitle(e)} />
+          <MDBInput label='Current Location' className='input' type='text' value={currentLocation} onChange={e => handleCurrentLocation(e)} />
+          <MDBDropdown animation>
+            <MDBDropdownToggle>{livingType || 'Living Type'}</MDBDropdownToggle>
+            <MDBDropdownMenu>
+              <MDBDropdownItem link onClick={e => handleLivingType(e)}>Rent</MDBDropdownItem>
+              <MDBDropdownItem link onClick={e => handleLivingType(e)}>Buy</MDBDropdownItem>
+              <MDBDropdownItem link onClick={e => handleLivingType(e)}>Lease</MDBDropdownItem>
+            </MDBDropdownMenu>
+        </MDBDropdown>
         </div>
       </main>
     </>
