65a6074be1bda279e655aa30dd07328c6a9a8faa
Luis F. Vega <vegaluis1220@gmail.com>
2023-03-25 18:33:33 -0400

    Got index.js to work with cockroachdb queries

--- src/backend/index.js
+++ src/backend/index.js
@@ -3,23 +3,13 @@ const { Client } = require('pg');
 
 const app = express();
 
-// const client = new Client({
-//   user: 'master',
-//   host: 'mycockroachdbcluster.db.elephantsql.com',
-//   database: 'defaultdb',
-//   password: 'BAESOAJl2xY2_ZvZHgrIPw',
-//   port: 26257,
-//   ssl: {
-//     rejectUnauthorized: false
-//   }
-// });
-
-const client = new Client(process.env.DATABASE_URL)
+const DATABASE_URL = 'postgresql://master:BAESOAJl2xY2_ZvZHgrIPw@hackabull23-9761.7tt.cockroachlabs.cloud:26257/defaultdb?sslmode=verify-full'
+const client = new Client(DATABASE_URL)
 
 client.connect();
 
 app.get('/', (req, res) => {
-  client.query('SELECT * FROM mytable', (err, result) => {
+  client.query('SELECT * FROM test;', (err, result) => {
     if (err) {
       console.error(err);
       res.send('An error occurred');
@@ -29,10 +19,6 @@ app.get('/', (req, res) => {
   });
 });
 
-// testing the db
-const selectStatement = `SELECT * FROM test;`
-
 app.listen(3001, () => {
   console.log('Server is listening on port 3001');
-  console.log(selectStatement);
 });
