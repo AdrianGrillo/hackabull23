39cc4cd07e910383ea8d06cbc923bad2e1bdc2bd
Jubbery <roberts28077@gmail.com>
2023-03-26 10:55:28 -0400

    UI updates

--- src/pages/get-started.js
+++ src/pages/get-started.js
@@ -1,6 +1,18 @@
-import React from 'react';
-import { Container, Typography, Box } from '@mui/material';
-import { keyframes } from '@mui/system';
+import React, { useState } from "react";
+import {
+  Container,
+  Typography,
+  Box,
+  TextField,
+  Button,
+  Stack,
+  Tab,
+  Tabs,
+  createTheme,
+} from "@mui/material";
+import { keyframes } from "@mui/system";
+import Link from "next/link";
+import theme from "@/styles/text-field-style";
 
 const fadeIn = keyframes`
   0% {
@@ -12,16 +24,51 @@ const fadeIn = keyframes`
 `;
 
 const GetStartedPage = () => {
+  const [value, setValue] = useState(0);
+  const [name, setName] = useState("");
+  const [dob, setDob] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [city, setCity] = useState("");
+  const [state, setState] = useState("");
+  const [desiredSalary, setDesiredSalary] = useState("");
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  const handleSignUp = async (e) => {
+    e.preventDefault();
+
+    // Send data to your API or backend server to store it in CockroachDB
+    // Example:
+    // const response = await fetch('/api/signup', {
+    //   method: 'POST',
+    //   headers: { 'Content-Type': 'application/json' },
+    //   body: JSON.stringify({ name, dob, email, password, city, state, desiredSalary }),
+    // });
+    //
+    // if (response.ok) {
+    //   // Redirect the user to the job searching page
+    //   // Example using Next.js router:
+    //   // import { useRouter } from 'next/router';
+    //   // const router = useRouter();
+    //   // router.push('/job-finder');
+    // }
+  };
+
+  // ... rest of the code
+
   return (
     <Container maxWidth="md">
       <Box
         sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-          minHeight: '100vh',
-          justifyContent: 'center',
-          textAlign: 'center',
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          minHeight: "100vh",
+          justifyContent: "center",
+          textAlign: "center",
           padding: 4,
         }}
       >
@@ -33,13 +80,160 @@ const GetStartedPage = () => {
             animation: `${fadeIn} 1s ease-in`,
           }}
         >
-          Welcome to your Fresh Start
+          Welcome to your FreshStart
         </Typography>
-        {/* Add your content here */}
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          indicatorColor="secondary"
+          textColor="inherit"
+          variant="fullWidth"
+        >
+          <Tab label="Sign Up" />
+          <Tab label="Log In" />
+        </Tabs>
+
+        {value === 0 && (
+          <Box
+            component="form"
+            onSubmit={handleSignUp}
+            sx={{
+              mt: 2,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              width: "100%",
+              animation: `${fadeIn} 1.5s ease-in`,
+            }}
+          >
+            <TextField
+              label="Name"
+              fullWidth
+              variant="outlined"
+              margin="normal"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              sx={theme}
+            />
+            <TextField
+              label="Date of Birth"
+              fullWidth
+              variant="outlined"
+              margin="normal"
+              type="date"
+              InputLabelProps={{ shrink: true }}
+              value={dob}
+              onChange={(e) => setDob(e.target.value)}
+              sx={theme}
+            />
+            <TextField
+              label="Email"
+              fullWidth
+              variant="outlined"
+              margin="normal"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              sx={theme}
+            />
+            <TextField
+              label="Password"
+              fullWidth
+              variant="outlined"
+              margin="normal"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              sx={theme}
+            />
+            <TextField
+              label="City"
+              fullWidth
+              variant="outlined"
+              margin="normal"
+              value={city}
+              onChange={(e) => setCity(e.target.value)}
+              sx={theme}
+            />
+            <TextField
+              label="State"
+              fullWidth
+              variant="outlined"
+              margin="normal"
+              value={state}
+              onChange={(e) => setState(e.target.value)}
+              sx={theme}
+            />
+            <TextField
+              label="Desired Salary"
+              fullWidth
+              variant="outlined"
+              margin="normal"
+              value={desiredSalary}
+              onChange={(e) => setDesiredSalary(e.target.value)}
+              sx={theme}
+            />
+            <Box
+              sx={{
+                mt: 2,
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              <Button
+                type="submit"
+                variant="contained"
+                color="primary"
+                sx={{ mt: 2 }}
+              >
+                Sign Up
+              </Button>
+              <Typography variant="h6" sx={{ mx: 2, mt: 2 }}>
+                Or
+              </Typography>
+
+              <Link href="/job-finder" passHref>
+                <Button variant="contained" color="primary" sx={{ mt: 2 }}>
+                  Skip
+                </Button>
+              </Link>
+            </Box>
+          </Box>
+        )}
+        {value === 1 && (
+          <Box
+            component="form"
+            sx={{
+              mt: 2,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              width: "100%",
+            }}
+          >
+            <TextField
+              label="Email"
+              fullWidth
+              variant="outlined"
+              margin="normal"
+              sx={theme}
+            />
+            <TextField
+              label="Password"
+              fullWidth
+              variant="outlined"
+              margin="normal"
+              type="password"
+              sx={theme}
+            />
+            <Button variant="contained" color="primary" sx={{ mt: 2 }}>
+              Log In
+            </Button>
+          </Box>
+        )}
       </Box>
     </Container>
   );
 };
 
 export default GetStartedPage;
-
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,42 +1,74 @@
 import React from "react";
 import Link from "next/link";
 import { Container, Typography, Box, Button } from "@mui/material";
+import { keyframes } from "@mui/system";
+
+const fadeIn = keyframes`
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+`;
 
 function Home() {
   return (
     <Container maxWidth="md">
       <Box
         sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-          minHeight: '90vh',
-          justifyContent: 'center',
-          textAlign: 'center',
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          minHeight: "100vh",
+          justifyContent: "center",
+          textAlign: "center",
           padding: 4,
         }}
       >
-        <Typography variant="h2" component="h1" gutterBottom>
+        <Typography
+          sx={{ animation: `${fadeIn} 1s ease-in` }}
+          variant="h2"
+          component="h1"
+          gutterBottom
+        >
           FreshStart
         </Typography>
-        <Typography variant="h5" component="p" gutterBottom>
+        <Typography
+          sx={{ animation: `${fadeIn} 1.5s ease-in` }}
+          variant="h5"
+          component="p"
+          gutterBottom
+        >
           Empowering New Beginnings
         </Typography>
-        <Typography variant="body1" component="p" gutterBottom>
-          FreshStart connects job seekers with affordable living opportunities, providing personalized recommendations and resources for a brighter future.
+        <Typography
+          sx={{ animation: `${fadeIn} 2s ease-in` }}
+          variant="body1"
+          component="p"
+          gutterBottom
+        >
+          FreshStart connects job seekers with affordable living opportunities,
+          providing personalized recommendations and resources for a brighter
+          future.
         </Typography>
         <Box sx={{ marginTop: 3 }}>
           <nav>
-          <Link href="/get-started" style={{ textDecoration: 'none' }} passHref>
-            <Button variant="contained" color="primary" size="large">
-              Get Started
-            </Button>
-          </Link>
+            <Link href="/get-started" passHref>
+              <Button
+                sx={{ animation: `${fadeIn} 2.5s ease-in` }}
+                variant="contained"
+                color="primary"
+                size="large"
+              >
+                Get Started
+              </Button>
+            </Link>
           </nav>
         </Box>
       </Box>
-      </Container>
-    );
+    </Container>
+  );
 }
 
 export default Home;
